6987da93b7cf32dc0ef93b5196dbdb9295e2afd6
nikapa21 <nikapa21@gmail.com>
2019-03-31 04:17:56 +0300

    -Init function first iteration

--- src/system/Demo.java
+++ src/system/Demo.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Demo {
@@ -21,12 +23,39 @@ public class Demo {
         List<String> busLinesList = new ArrayList<>();
         busLinesList.add("025");
 
-        Publisher publisher = new Publisher(busLinesList);
-        publisher.init(7777);
+        //Publisher publisher = new Publisher(busLinesList);
+        Set<String> allVehicles = findAllVehicleIds();
+
+        for(String vehicleId : allVehicles){
+            Publisher publisher= new Publisher();
+            System.out.println("Vehicle " + vehicleId);
+            publisher.init(Integer.parseInt(vehicleId.trim())); // .trim giati eskage se kapoio lathos(?) tou dataset
+        }
+        System.out.println("Found " + allVehicles.size() + " vehicles");
 
         Subscriber subscriber = new Subscriber();
 
         // start broker 1
 
     }
+
+    public static Set<String> findAllVehicleIds(){
+        String busPositionsFile = "./Dataset/DS_project_dataset/busPositionsNew.txt";
+        Set<String> distinctVehicleIds = new HashSet<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(busPositionsFile))) {
+
+            stream.forEach(line -> {
+                        String[] fields = line.split(",");
+                        String vehicle = fields[2];
+                        distinctVehicleIds.add(vehicle);
+                    });
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return distinctVehicleIds;
+    }
+
 }
--- src/system/instances/Publisher.java
+++ src/system/instances/Publisher.java
@@ -7,28 +7,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Publisher implements Node{
 
     List<Topic> topics = new ArrayList<>();
 
-
     List<BusLine> publisherBusLines = new ArrayList<>();
     List<RouteCode> publisherRouteCodes = new ArrayList<>();
     List<BusPosition> publisherBusPositions = new ArrayList<>();
-
-    public Publisher(List<String> list) {
-        for (String busLine : list) {
-            topics.add(new Topic(busLine));
-        }
-    }
+//
+//    public Publisher(List<String> list) {
+//        for (String busLine : list) {
+//            topics.add(new Topic(busLine));
+//        }
+//    }
 //    void getBrokerList();
 //    Broker hashTopic(Topic topic);
 //    void push(Topic topic, Value value);
 //    void notifyFailure(Broker broker);
 
-    public void init(int port){
+    public void init(int vehicleId){ //10374
         //O
         //publisher node κατά την έναρξη της λειτουργίας του θα πρέπει να γνωρίζει για
         //ποια κλειδιά είναι υπεύθυνος καθώς επίσης και όλη την απαραίτητη
@@ -38,22 +38,44 @@ public class Publisher implements Node{
 
         // for example topics responsible for 025, 026, 027
 
-        publisherBusLines = findFromBusLinesFile(topics);
-        publisherRouteCodes = findFromRouteCodesFile(publisherBusLines);
-        publisherBusPositions = findFromBusPositionsFile(publisherRouteCodes);
+        //personList.stream()
+        //  .filter(distinctByKey(p -> p.getName()))
+        //  .collect(Collectors.toList());
+
+        // Me vasi to vehicle id vres ola ta bus position objects apo to arxeio me ta bus positions
+        publisherBusPositions = findFromBusPositionsFile(vehicleId);
+
+        // Vres ola ta distinct route codes apo ta parapanw bus position objects
+        List<String> distinctRouteCodes = publisherBusPositions.stream().map(BusPosition::getRouteCode).distinct().collect(Collectors.toList());
+
+        // Apo ta distinct route codes Strings vres ola ta route code objects apo to arxeio me ta route codes
+        publisherRouteCodes = findFromRouteCodesFile(distinctRouteCodes);
+
+        // Vres ola ta distinct line codes apo ta parapanw route code objects
+        List<String> distinctLineCodes = publisherRouteCodes.stream().map(RouteCode::getLineCode).distinct().collect(Collectors.toList());
+
+        // Apo ta distinct bus lines Strings vres ola ta bus line objects apo to arxeio me ta bus lines
+        publisherBusLines = findFromBusLinesFile(distinctLineCodes);
+
+        System.out.print("Vehicle with id " + vehicleId + " is responsible for the following lines/topics: ");
+        for(BusLine busLine: publisherBusLines){
+            topics.add(new Topic(busLine.getLineId()));
+            System.out.print(busLine.getLineId() + " ");
+        }
+        System.out.println();
     }
 
-    private List<BusLine> findFromBusLinesFile(List<Topic> topics) {
+    private List<BusLine> findFromBusLinesFile(List<String> busLines) {
 
         String busLinesFile = "./Dataset/DS_project_dataset/busLinesNew.txt";
         List<BusLine> publisherBusLines = new ArrayList<>();
 
         //read file into stream, try-with-resources
 
-        for (Topic topic : topics) {
+        for (String busLine : busLines) {
             try (Stream<String> stream = Files.lines(Paths.get(busLinesFile))) {
 
-                stream.filter(line -> line.contains(topic.getBusLine()))
+                stream.filter(line -> line.contains(busLine + ","))
                         .forEach(line -> {
                             String[] fields = line.split(",");
                             BusLine myBusLine = new BusLine(fields[0], fields[1], fields[2]);
@@ -69,23 +91,21 @@ public class Publisher implements Node{
 
     }
 
-    private List<RouteCode> findFromRouteCodesFile(List<BusLine> publisherBusLines) {
+    private List<RouteCode> findFromRouteCodesFile(List<String> routeCodes) {
 
         String routeCodesFile = "./Dataset/DS_project_dataset/RouteCodesNew.txt";
         List<RouteCode> publisherRouteCodes = new ArrayList<>();
 
         //read file into stream, try-with-resources
-        for(BusLine busLine:publisherBusLines) {
+        for(String routeCode : routeCodes) {
             try (Stream<String> stream = Files.lines(Paths.get(routeCodesFile))) {
 
-
-                stream.filter(line -> line.contains(busLine.getLineCode()))
+                stream.filter(line -> line.contains(routeCode + ","))
                         .forEach(line -> {
                             String[] fields = line.split(",");
-                            RouteCode routeCode = new RouteCode(fields[0], fields[1], fields[2]);
-                            publisherRouteCodes.add(routeCode);
+                            RouteCode routeCodeObject = new RouteCode(fields[1], fields[0], fields[3]);
+                            publisherRouteCodes.add(routeCodeObject);
                         });
-
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -95,26 +115,23 @@ public class Publisher implements Node{
         return publisherRouteCodes;
     }
 
-    private List<BusPosition> findFromBusPositionsFile(List<RouteCode> publisherRouteCodes) {
+    private List<BusPosition> findFromBusPositionsFile(int vehicleId) {
 
         String busPositionsFile = "./Dataset/DS_project_dataset/busPositionsNew.txt";
         List<BusPosition> publisherBusPositions = new ArrayList<>();
 
-        for (RouteCode routeCode : publisherRouteCodes) {
-            //read file into stream, try-with-resources
-            try (Stream<String> stream = Files.lines(Paths.get(busPositionsFile))) {
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(busPositionsFile))) {
 
+            stream.filter(line -> line.contains(String.valueOf(vehicleId)+ ","))
+                    .forEach(line -> {
+                        String[] fields = line.split(",");
+                        BusPosition busPosition = new BusPosition(fields[0], fields[1], fields[2], Double.parseDouble(fields[3]), Double.parseDouble(fields[4]), fields[5]);
+                        publisherBusPositions.add(busPosition);
+                    });
 
-                stream.filter(line -> line.contains(routeCode.getRouteCode()))
-                        .forEach(line -> {
-                            String[] fields = line.split(",");
-                            BusPosition busPosition = new BusPosition(fields[0], fields[1], fields[2], Double.parseDouble(fields[3]), Double.parseDouble(fields[4]), fields[5]);
-                            publisherBusPositions.add(busPosition);
-                        });
-
-            } catch(IOException e){
-                e.printStackTrace();
-            }
+        } catch(IOException e){
+            e.printStackTrace();
         }
 
         return publisherBusPositions;
